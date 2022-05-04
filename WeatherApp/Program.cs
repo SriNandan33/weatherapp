49fa80f2e9aa87852d0dd46cd6e7053b0d47fb20
Srinivasa Rao <srinivasarao@seekasia.com>
2022-05-04 17:33:04 +0530

    revoke api key, and add a place holder

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var apiKey = "d07fd64cd5c5495face164104221904";
+var apiKey = "<your-api-key>";
 var today = DateTime.Now;
 var lastWeekDate = DateTime.Now.AddDays(-5);
 var avgTemp = new WeatherApp.WeatherApp(apiKey).GetAvgTempBetween(lastWeekDate, today);
